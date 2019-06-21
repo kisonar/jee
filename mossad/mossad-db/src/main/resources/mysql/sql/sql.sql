c543fddcf9d3fb7092f3158db94f01ae8daafd49
Marcin Migdal <marcin.migdal@nokia.com>
2019-06-21 16:29:44 +0200

    Works MYSQL

---
+++ mossad/mossad-db/src/main/resources/mysql/sql/sql.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE marcin;
+USE marcin;
+commit;
+
+CREATE TABLE mandingo (
+    first_name varchar(25),
+    last_name  varchar(25),
+    department varchar(15),
+    email  varchar(50)
+);
+commit;
