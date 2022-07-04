fe42097d521c0f3988ef52cd1ba80679b5a463aa
Marcin Migdal <marcinmigdal@wp.pl>
2022-07-04 14:48:18 +0200

    Update dependencies: JSF update -> breaking dependencies

--- clients/web/jsf/src/main/java/com/mossad/util/i18n/RememberLanguageBean.java
+++ clients/web/jsf/src/main/java/com/mossad/util/i18n/RememberLanguageBean.java
@@ -24,10 +24,6 @@ public class RememberLanguageBean extends CookiesOperations implements Serializa
         return language;
     }
 
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
     /**
      * Saves selected language to cookie.
      *
@@ -35,13 +31,6 @@ public class RememberLanguageBean extends CookiesOperations implements Serializa
      */
     public void saveLanguageToCookie(String language) {
         saveStringToCookie(LANGUAGE_COOKIE_NAME, language);
-    }
-
-    /**
-     * Deletes language cookie.
-     */
-    public void deleteLanguageCookie() {
-        deleteCookie(LANGUAGE_COOKIE_NAME);
     }
 
     private void checkLanguageCookie() {
